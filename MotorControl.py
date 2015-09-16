06c4bd72e124d494756013d5d91c0cfd03f7922d
RMcGhee <RMcGhee@users.noreply.github.com>
2015-09-15 20:38:48 -0500

    Will be worked on when hardware integration starts

--- MotorControl.py
+++ MotorControl.py
@@ -1,6 +1,6 @@
 #MotorControl controls the motors used on the mount. These motors are threaded
 #to allow moves in all three directions at the same time.
-
+#this will be worked on when the hardware integration starts.
 import math
 import threading
 
